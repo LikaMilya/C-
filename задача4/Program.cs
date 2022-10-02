46007decd2caf5b087546366b0a05b5edc0c3d51
Lika <anhelika3432@mail.ru>
2022-10-02 23:50:06 +0600

    решила задачу четвертую

---
+++ задача4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Напишите программу, которая на выход принимает три  числа и выдает, какое число больше.
+Console.Clear();
+Console.WriteLine("Введите первое чило");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе чило");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье чило");
+int c = int.Parse(Console.ReadLine()!);
+int max = x;
+if (y > max)
+     max = y;
+if (c > max)
+     max = c;
+Console.WriteLine($"Максимальное{max}");
